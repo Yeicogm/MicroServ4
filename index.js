f6c932c79cbfdebd6deb7e5fb40d78cc6f12580c
Yeico Med <yeicogm@gmail.com>
2024-12-04 10:40:29 +0000

    api user

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-require('dotenv').config()
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+//esquemas
+const userSchema = new Schema({
+  _id: {type: Schema.Types.ObjectId, auto: true},
+  username: { type: String, require: true },
+  log: []
+});
+const User = mongoose.model('User', userSchema);
+
+
+//conector modo con datos de .env
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false })); 
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-
-
-
+app.post('/api/users', (req, res) => {
+	const myUser = new User({ username: req.body.username});
+	myUser.save()
+		.then(item => {
+			res.json({ username: item.username, _id: item.id });
+		})
+		.catch(err => {
+			res.status(400).send("Sorry, we couldn't save this user to the db");
+		});
+})
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
